a274a69e52b2fc056c5964b4cad00777ddd96bbf
Marko Puntaric <puntaric@domain.local>
2021-11-15 11:58:22 +0100

    Doc update

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
     client.subscribe(topic)
     client.on_message = on_message
 
